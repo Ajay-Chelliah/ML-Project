990261124d22412a976a96fa68059f5e0b27b9eb
Ajay-Chelliah <ajay8425p@gmail.com>
2024-09-20 00:25:41 +0530

    Added comments for bettter understanding

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,17 +8,33 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-@dataclass
+
+@dataclass  # This abstract method helps us to create class variables without a __init__ function
 class DataIngestionConfig:
+    '''
+     This class is used to create class variables to store the path name of the data files 
+     which includes directory name and file name.
+    '''
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"raw.csv")
 
 class DataIngestion:
-    def __init__(self):
+    """
+    This class is to perform the data ingestion steps
+    """
+    def __init__(self): # Creates an object for the above config class to access the file paths.
         self.ingestion_config = DataIngestionConfig()
     
-    def initiate_data_ingestion(self):
+    def initiate_data_ingestion(self): 
+        """
+            1. Reading the raw data
+            2. Creating the directory mentioned in the config class if not exist
+            3. Saving the raw data as raw.csv inside the artifact directory
+            4. Splitting the raw data into train and test data and saving them as train.csv and test.csv in artifacts folder.
+            5. Returning the train and test data path so that those can be accessed by the other py files like data_transformation.
+            6. Logging is included in all the important steps to keep track of the steps executed to find and solve any error easily.
+        """
         logging.info("Enter the data ingestion method or component")
         try:
             df = pd.read_csv("notebook/data/stud.csv")
@@ -44,10 +60,16 @@ class DataIngestion:
                 self.ingestion_config.test_data_path,
             )
 
-        except Exception as e:
+        except Exception as e: # Raise a custom exception defined in the exception.py file
             raise CustomException(e,sys)
 
 if __name__ == "__main__":
+    """
+        Creates an object for the main class DataIngestion 
+        Calls the important function inside it to initiate the data ingestion process
+        Creates an object for the Data Transformation class
+        Call the function inside the above class to transform the data (numerical and categorical)
+    """
     obj=DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,13 +16,25 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
+    """
+        Like in the data ingestion file , we create a variable to store the file path where the preprocessor will get stored.
+    """
     preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
 
 class DataTransformation:
-    def __init__(self):
+    """
+        This class is to perform the transformation steps like scaling and encoding.
+    """
+    def __init__(self): # Creating a object of the above config class to access the path variable.
         self.DataTransformation_config = DataTransformationConfig()
 
     def get_data_transformer_obj(self):
+        """
+            1. Finding the numerical and categorical columns.
+            2. Createing a Pipeline for transforming both numerical and categorical columns seperately.
+            3. Creating a Column Transformer to run the steps in a correct order.
+            4. Returns the Column Transformer variable (preprocessor).
+        """
         try:
             num_cols = ['writing_score','reading_score']
             cat_cols = ['gender','race_ethnicity','parental_level_of_education','lunch','test_preparation_course']
@@ -57,6 +69,16 @@ class DataTransformation:
             raise CustomException(e,sys)
         
     def initiate_data_transformation(self,train_path,test_path):
+        """
+            1. Reading the train and test file.
+            2. Getting the preprocessor from the above function.
+            3. Finding the input and target columns for both train and test data.
+            4. Fitting and transforming the input data using the preprocessor.
+            5. Concatenating the input and target columns column-wise with the respective rows for both train and test data.
+            6. Saving the preprocessor object.
+            7. Returning the transformed train and test data and the preprocessor object.
+            8. Logging all the important steps.
+        """
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
@@ -87,6 +109,10 @@ class DataTransformation:
                 file_path=self.DataTransformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
             )
+            """
+                This is a function defined in the util.py file 
+                which create a mentioned directory and store the preprocessor object using dill module.
+            """
 
             return(
                 train_arr,
