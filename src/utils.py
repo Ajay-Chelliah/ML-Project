d004a7cf7d2d82723d70e8a14400387ec3e6d74f
Ajay-Chelliah <ajay8425p@gmail.com>
2024-09-22 16:03:13 +0530

    Webapp for prediction

--- src/utils.py
+++ src/utils.py
@@ -19,6 +19,13 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
 def evaluate_model(X_train, Y_train , X_test , Y_test ,models):
     try:
         report = {}
